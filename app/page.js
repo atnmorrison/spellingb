79c279f531126f6df6b5012a42557cf0c3e0e63c
Scott Morrison <scott.morrison@qlik.com>
2024-03-07 08:47:47 -0700

    updated privacy policy

--- app/page.js
+++ app/page.js
@@ -1,13 +1,1 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-
-      <div className="container mx-auto items-center">
-        <h1>Spelling B</h1>
-      </div>
-
-    </main>
-  );
-}
--- app/privacypolicy/page.js
+++ app/privacypolicy/page.js
@@ -8,7 +8,7 @@ export default function Home() {
           <h1>Privacy Policy</h1>
   
           <div>
-              This application does not collect or store personal data.
+              This privacy policy applies to the Spelling B android google play app. The app does not collect or store personal data.
           </div>
           <div>
               If you installed this application from Google Play, then Google Play does collect some personal data. Please see the privacy policy for Google Play Services here: <a href="https://policies.google.com/privacy">https://policies.google.com/privacy</a>          
